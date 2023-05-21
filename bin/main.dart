7a993abe9de69f1c3fcb1545757b091d76de612d
cjl_macbook <cjl_spy@163.com>
2023-05-21 21:11:06 +0800

    feat: update bin/main.dart

--- bin/main.dart
+++ bin/main.dart
@@ -2,8 +2,18 @@ import 'package:github_action_context/github_action_context.dart';
 import 'package:github_action_core/github_action_core.dart';
 
 void main(List<String> arguments) {
+  print('context.eventName: ${context.eventName}');
+  print('context.sha: ${context.sha}');
+  print('context.ref: ${context.ref}');
+  print('context.workflow: ${context.workflow}');
   print('context.action: ${context.action}');
   print('context.actor: ${context.actor}');
+  print('context.job: ${context.job}');
+  print('context.runNumber: ${context.runNumber}');
+  print('context.runId: ${context.runId}');
+  print('context.apiUrl: ${context.apiUrl}');
+  print('context.serverUrl: ${context.serverUrl}');
+  print('context.graphqlUrl: ${context.graphqlUrl}');
 
   startGroup('context.payload');
   info(context.payload.toString());
